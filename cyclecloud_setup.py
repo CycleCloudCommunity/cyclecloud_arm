5e2141cb39a1c54e502a4f1ad75c07fb6d9ca279
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2018-08-29 17:19:22 +0800

    Update deploy to use CycleCloud Marketplace Image

---
+++ cyclecloud_setup.py
@@ -0,0 +1,324 @@
+#!/usr/bin/python
+# Prepare an Azure provider account for CycleCloud usage.
+import argparse
+import tarfile
+import json
+import re
+import random
+from string import ascii_letters, ascii_uppercase, ascii_lowercase, digits
+from subprocess import CalledProcessError, check_output
+from os import path, listdir, makedirs, chdir, fdopen, remove
+from urllib2 import urlopen, Request, HTTPError, URLError
+from urllib import urlretrieve
+from shutil import rmtree, copy2, move, copytree
+from tempfile import mkstemp, mkdtemp
+from time import sleep
+
+
+tmpdir = mkdtemp()
+print "Creating temp directory " + tmpdir + " for installing CycleCloud"
+cycle_root = "/opt/cycle_server"
+cs_cmd = cycle_root + "/cycle_server"
+
+
+def clean_up():
+    rmtree(tmpdir)
+
+def _catch_sys_error(cmd_list):
+    try:
+        output = check_output(cmd_list)
+        print cmd_list
+        print output
+    except CalledProcessError as e:
+        print "Error with cmd: %s" % e.cmd
+        print "Output: %s" % e.output
+        raise
+
+def create_user_credential(username):
+    authorized_keyfile = "/home/" + username + "/.ssh/authorized_keys"
+    public_key = ""
+    with open(authorized_keyfile, 'r') as pubkeyfile:
+        public_key = pubkeyfile.read()
+
+    credential_record = {
+        "PublicKey": public_key,
+        "AdType": "Credential",
+        "CredentialType": "PublicKey",
+        "Name": username + "/public"
+    }
+    credential_data_file = tmpdir + "/credential.json"
+    with open(credential_data_file, 'w') as fp:
+        json.dump(credential_record, fp)
+
+    copy2(credential_data_file, cycle_root + "/config/data/")
+
+def account_and_cli_setup(vm_metadata, tenant_id, application_id, application_secret, admin_user, azure_cloud, accept_terms, password):
+    print "Setting up azure account in CycleCloud and initializing cyclecloud CLI"
+
+    subscription_id = vm_metadata["compute"]["subscriptionId"]
+    location = vm_metadata["compute"]["location"]
+    resource_group = vm_metadata["compute"]["resourceGroupName"]
+
+    random_suffix = ''.join(random.SystemRandom().choice(
+        ascii_lowercase) for _ in range(14))
+
+    cyclecloud_admin_pw = ""
+    
+    if password:
+        print 'Password specified, using it as the admin password'
+        cyclecloud_admin_pw = password
+    else:
+        random_pw_chars = ([random.choice(ascii_lowercase) for _ in range(20)] +
+                        [random.choice(ascii_uppercase) for _ in range(20)] +
+                        [random.choice(digits) for _ in range(10)])
+        random.shuffle(random_pw_chars)
+        cyclecloud_admin_pw = ''.join(random_pw_chars)
+
+    storage_account_name = 'cyclecloud' + random_suffix
+    azure_data = {
+        "Environment": azure_cloud,
+        "AzureRMApplicationId": application_id,
+        "AzureRMApplicationSecret": application_secret,
+        "AzureRMSubscriptionId": subscription_id,
+        "AzureRMTenantId": tenant_id,
+        "AzureResourceGroup": resource_group,
+        "DefaultAccount": True,
+        "Location": location,
+        "Name": "azure",
+        "Provider": "azure",
+        "ProviderId": subscription_id,
+        "RMStorageAccount": storage_account_name,
+        "RMStorageContainer": "cyclecloud"
+    }
+
+    app_setting_installation = {
+        "AdType": "Application.Setting",
+        "Name": "cycleserver.installation.complete",
+        "Value": True
+    }
+    initial_user = {
+        "AdType": "Application.Setting",
+        "Name": "cycleserver.installation.initial_user",
+        "Value": admin_user
+    }
+    authenticated_user = {
+        "AdType": "AuthenticatedUser",
+        "Name": 'root',
+        "RawPassword": cyclecloud_admin_pw,
+        "Superuser": True
+    }
+    account_data = [
+        authenticated_user,
+        initial_user,
+        app_setting_installation
+    ]
+
+    if accept_terms:
+        # Terms accepted, auto-create login user account as well
+        login_user = {
+            "AdType": "AuthenticatedUser",
+            "Name": admin_user,
+            "RawPassword": cyclecloud_admin_pw,
+            "Superuser": True
+        }
+        account_data.append(login_user)
+
+    account_data_file = tmpdir + "/account_data.json"
+    azure_data_file = tmpdir + "/azure_data.json"
+
+    with open(account_data_file, 'w') as fp:
+        json.dump(account_data, fp)
+
+    with open(azure_data_file, 'w') as fp:
+        json.dump(azure_data, fp)
+
+    copy2(account_data_file, cycle_root + "/config/data/")
+
+    # wait for the data to be imported
+    password_flag = ("--password=%s" % cyclecloud_admin_pw)
+    sleep(5)
+
+    print "Initializing cyclecloud CLI"
+    _catch_sys_error(["/usr/local/bin/cyclecloud", "initialize", "--loglevel=debug", "--batch",
+                      "--url=https://localhost", "--verify-ssl=false", "--username=root", password_flag])
+
+    homedir = path.expanduser("~")
+    cycle_config = homedir + "/.cycle/config.ini"
+    with open(cycle_config, "a") as config_file:
+        config_file.write("\n")
+        config_file.write("[pogo azure-storage]\n")
+        config_file.write("type = az\n")
+        config_file.write("subscription_id = " + subscription_id + "\n")
+        config_file.write("tenant_id = " + tenant_id + "\n")
+        config_file.write("application_id = " + application_id + "\n")
+        config_file.write("application_secret = " + application_secret + "\n")
+        config_file.write("matches = az://" +
+                          storage_account_name + "/cyclecloud" + "\n")
+
+    print "Registering Azure subscription"
+    # create the cloud provide account
+    _catch_sys_error(["/usr/local/bin/cyclecloud", "account",
+                      "create", "-f", azure_data_file])
+
+    # create a pogo.ini for the admin_user so that cyclecloud project upload works
+    admin_user_cycledir = "/home/" + admin_user + "/.cycle"
+    if not path.isdir(admin_user_cycledir):
+        makedirs(admin_user_cycledir, mode=700)
+
+    pogo_config = admin_user_cycledir + "/pogo.ini"
+
+    with open(pogo_config, "w") as pogo_config:
+        pogo_config.write("[pogo azure-storage]\n")
+        pogo_config.write("type = az\n")
+        pogo_config.write("subscription_id = " + subscription_id + "\n")
+        pogo_config.write("tenant_id = " + tenant_id + "\n")
+        pogo_config.write("application_id = " + application_id + "\n")
+        pogo_config.write("application_secret = " + application_secret + "\n")
+        pogo_config.write("matches = az://" +
+                          storage_account_name + "/cyclecloud" + "\n")
+
+    _catch_sys_error(["chown", "-R", admin_user, admin_user_cycledir])
+    _catch_sys_error(["chmod", "-R", "700", admin_user_cycledir])
+
+    if not accept_terms:
+        # reset the installation status so the splash screen re-appears
+        print "Resetting installation"
+        sql_statement = 'update Application.Setting set Value = false where name ==\"cycleserver.installation.complete\"'
+        _catch_sys_error(
+            ["/opt/cycle_server/cycle_server", "execute", sql_statement])
+
+
+def letsEncrypt(fqdn, location):
+    # FQDN is assumed to be in the form: hostname.location.cloudapp.azure.com
+    # fqdn = hostname + "." + location + ".cloudapp.azure.com"
+    sleep(60)
+    try:
+        cmd_list = [cs_cmd, "keystore", "automatic", "--accept-terms", fqdn]
+        output = check_output(cmd_list)
+        print cmd_list
+        print output
+    except CalledProcessError as e:
+        print "Error getting SSL cert from Lets Encrypt"
+        print "Proceeding with self-signed cert"
+
+
+def get_vm_metadata():
+    metadata_url = "http://169.254.169.254/metadata/instance?api-version=2017-08-01"
+    metadata_req = Request(metadata_url, headers={"Metadata": True})
+
+    for i in range(30):
+        print "Fetching metadata"
+        metadata_response = urlopen(metadata_req, timeout=2)
+
+        try:
+            return json.load(metadata_response)
+        except ValueError as e:
+            print "Failed to get metadata %s" % e
+            print "    Retrying"
+            sleep(2)
+            continue
+        except:
+            print "Unable to obtain metadata after 30 tries"
+            raise
+
+
+def start_cc():
+    print "Starting CycleCloud server"
+    _catch_sys_error([cs_cmd, "start"])
+    _catch_sys_error([cs_cmd, "await_startup"])
+    _catch_sys_error([cs_cmd, "status"])
+
+
+def modify_cs_config():
+    print "Editing CycleCloud server system properties file"
+    # modify the CS config files
+    cs_config_file = cycle_root + "/config/cycle_server.properties"
+
+    fh, tmp_cs_config_file = mkstemp()
+    with fdopen(fh, 'w') as new_config:
+        with open(cs_config_file) as cs_config:
+            for line in cs_config:
+                if line.startswith('webServerMaxHeapSize='):
+                    new_config.write('webServerMaxHeapSize=4096M')
+                elif line.startswith('webServerPort='):
+                    new_config.write('webServerPort=80')
+                elif line.startswith('webServerSslPort='):
+                    new_config.write('webServerSslPort=443')
+                elif line.startswith('webServerEnableHttps='):
+                    new_config.write('webServerEnableHttps=true')
+                else:
+                    new_config.write(line)
+
+    remove(cs_config_file)
+    move(tmp_cs_config_file, cs_config_file)
+
+    #Ensure that the files are created by the cycleserver service user
+    _catch_sys_error(["chown", "-R", "cycle_server.", cycle_root])
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="usage: %prog [options]")
+
+    parser.add_argument("--cyclecloudVersion",
+                        dest="cyclecloudVersion",
+                        #   required=True,
+                        help="CycleCloud version to install")
+
+    parser.add_argument("--downloadURL",
+                        dest="downloadURL",
+                        #   required=True,
+                        help="Download URL for the Cycle install")
+
+    parser.add_argument("--azureSovereignCloud",
+                        dest="azureSovereignCloud",
+                        help="Azure Region [china|germany|public|usgov]")
+
+    parser.add_argument("--tenantId",
+                        dest="tenantId",
+                        help="Tenant ID of the Azure subscription")
+
+    parser.add_argument("--applicationId",
+                        dest="applicationId",
+                        help="Application ID of the Service Principal")
+
+    parser.add_argument("--applicationSecret",
+                        dest="applicationSecret",
+                        help="Application Secret of the Service Principal")
+
+    parser.add_argument("--username",
+                        dest="username",
+                        help="The local admin user for the CycleCloud VM")
+
+    parser.add_argument("--hostname",
+                        dest="hostname",
+                        help="The short public hostname assigned to this VM (or public IP), used for LetsEncrypt")
+
+    parser.add_argument("--acceptTerms",
+                        dest="acceptTerms",
+                        action="store_true",
+                        help="Accept Cyclecloud terms and do a silent install")
+
+    parser.add_argument("--password",
+                        dest="password",
+                        help="The password for the CycleCloud UI user")
+
+    args = parser.parse_args()
+
+    print("Debugging arguments: %s" % args)
+
+    modify_cs_config()
+    start_cc()
+
+    vm_metadata = get_vm_metadata()
+    
+    letsEncrypt(args.hostname, vm_metadata["compute"]["location"])
+    account_and_cli_setup(vm_metadata, args.tenantId, args.applicationId,
+                          args.applicationSecret, args.username, args.azureSovereignCloud, args.acceptTerms, args.password)
+    create_user_credential(args.username)
+    clean_up()
+
+
+if __name__ == "__main__":
+    main()
+
